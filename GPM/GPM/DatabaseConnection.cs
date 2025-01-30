59dd61f1ff61a2d9a2669121b90db88f7ae5be61
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-01-30 19:21:00 +0530

    perf: Database Setup

---
+++ GPM/GPM/DatabaseConnection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GPM
+{
+    internal class DatabaseConnection
+    {
+        private MySqlConnection connection;
+
+        public DatabaseConnection()
+        {
+            string connectionString = "Server=127.0.0.1;Database=gpmdb;Uid=root;Pwd=78563;";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+
+        }
+    }
+}
