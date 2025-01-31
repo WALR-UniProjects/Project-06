ac5b948a4fd609e81c269c1f3aa67e24be067e01
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-01-31 14:39:35 +0530

    Update Program.cs

--- GPM/GPM/Program.cs
+++ GPM/GPM/Program.cs
@@ -16,7 +16,7 @@ namespace GPM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Employee_Allocation());
         }
     }
 }
