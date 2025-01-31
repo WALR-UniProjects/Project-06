576968805fc9b4ffaa3541015a2bd2a77540a5df
Lasitha Ransara <114856830+LasithaR01@users.noreply.github.com>
2025-01-31 18:20:53 +0530

    feat: Create Advanced Payment Details

---
+++ GPM/GPM/Advance Payment Details.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPM
+{
+    public partial class Advance_Payment_Details : Form
+    {
+        public Advance_Payment_Details()
+        {
+            InitializeComponent();
+        }
+    }
+}
